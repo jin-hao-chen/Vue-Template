8c0a9d75a86b48be7bb05dc9fc5dafc95dd60ec8
jh <jin_hao_chen@163.com>
2019-07-02 18:30:59 +0800

    修改了配置

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,14 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { VueLoaderPlugin } = require('vue-loader');
 
-var pathToClean = ['./dist'];
+var pathToClean = ['./build'];
 
 module.exports = {
     mode: 'development',
     entry: path.resolve(__dirname, 'src/main.js'),
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        path: path.resolve(__dirname, 'build'),
+        filename: 'app.js'
     },
     plugins: [
         new CleanWebpackPlugin(),
@@ -22,9 +22,22 @@ module.exports = {
         new VueLoaderPlugin()
     ],
     module: {
-        rules: [
-            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+        rules: [{
+                test: /\.css$/,
+                use: ['style-loader', {
+                    loader: 'css-loader',
+                    options: {
+                        modules: {
+                            localIdentName: '[name]_[local]-[hash:base64:5]',
+                        }
+                    }
+                }],
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
             { test: /\.vue$/, use: 'vue-loader' },
             {
                 test: /\.(ttf|svg|wof)$/,
